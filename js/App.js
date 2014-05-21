422175b422fe9f0f303c848206f23eab0b3fe53a
basha502 <basha502@gmail.com>
2014-05-21 22:56:55 +0530

    few changes

--- js/App.js
+++ js/App.js
@@ -1,17 +1,19 @@
-(function ($) {
+(function($) {
 
-	$.fn.accordion = function() {
-		this.on("click", ".accordion-heading a", function(e) {
-			e.preventDefault();
-			var hash = e.target.href.split('#').pop();
-			var body =  $(e.target).parent().next();
-			body.slideToggle();
-		});
-		return this;
-	};
+    $.fn.accordion = function() {
+        $(".collapse").not(".in").show();
+
+        this.on("click", ".accordion-heading a", function(e) {
+            e.preventDefault();
+            var hash = e.target.href.split('#').pop();
+            var body = $(e.target).parent().next();
+            body.slideToggle();
+        });
+        return this;
+    };
 
 })(jQuery);
 
 $(function() {
-	$("#container").accordion();
+    $("#container").accordion();
 });
