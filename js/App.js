186b9bae0dde48ce99b5d895077c901299424bcd
theblenderspride <basha502@gmail.com>
2014-05-21 11:54:56 +0530

    few changes

---
+++ js/App.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+	$.fn.accordion = function() {
+		this.on("click", ".accordion-heading a", function(e) {
+			e.preventDefault();
+			var hash = e.target.href.split('#').pop();
+			var body =  $(e.target).parent().next();
+			body.slideToggle();
+		});
+		return this;
+	};
+
+})(jQuery);
+
+$(function() {
+	$("#container").accordion();
+});
